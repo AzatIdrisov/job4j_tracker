a769ab94a2bd175c710e90c5e129451f313d1f29
Azat Idrisov <azzzat72@yandex.ru>
2020-07-13 17:04:00 +0500

    1.7. Состояние объекта [#321266]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -4,8 +4,8 @@ public class Battery {
     private int load;
 
     public void exchange(Battery another) {
-        this.load -= 5;
-        another.load += 5;
+        another.load += this.load;
+        this.load = 0;
     }
 
 }

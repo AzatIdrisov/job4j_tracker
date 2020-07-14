217e53a3b402f91326f27d0061ea65c6732ce14f
Azat Idrisov <azzzat72@yandex.ru>
2020-07-14 18:20:13 +0500

    0. Что такое инкапсуляция. [#321306]

--- src/main/java/ru/job4j/oop/Builder.java
+++
@@ -1,6 +0,0 @@
-package ru.job4j.oop;
-
-public class Builder extends Engineer {
-    String [] instruments;
-    public Building build (){}
-}
--- src/main/java/ru/job4j/oop/Doctor.java
+++
@@ -1,6 +0,0 @@
-package ru.job4j.oop;
-
-public class Doctor extends Profession {
-    String profOrientation;
-    public Diagnos heal(Pacient pacient) {}
-}

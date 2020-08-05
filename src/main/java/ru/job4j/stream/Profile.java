3a945a19bfddbdba3c6c06f0994cbda8b8dbedd7
Azat Idrisov <azzzat72@yandex.ru>
2020-08-05 21:07:09 +0500

    2. Список адресов [#321053]

--- src/main/java/ru/job4j/stream/Profile.java
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -7,12 +7,11 @@ import java.util.stream.Collectors;
 public class Profile {
     private Address address;
 
-    public List<Address> collect(List<Profile> profiles) {
-        Comparator<Address> city = (first, second) -> first.getCity().compareTo(second.getCity());
-        return profiles.stream()
-                .map(profile -> profile.address)
-                .sorted(city)
-                .distinct()
-                .collect(Collectors.toList());
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
     }
 }

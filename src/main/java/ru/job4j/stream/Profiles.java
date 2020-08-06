939a988693b4d2ce348e680f064146c4746f6225
Azat Idrisov <azzzat72@yandex.ru>
2020-08-06 20:26:28 +0500

    3. Уникальность элементов и сортировка. [#321054]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,10 +1,20 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(profile -> profile.getAddress()).collect(Collectors.toList());
+    }
+
+    public List<Address> collectUnique(List<Profile> profiles) {
+        Comparator<Address> city = (first, second) -> first.getCity().compareTo(second.getCity());
+        return profiles.stream()
+                .map(profile -> profile.getAddress())
+                .sorted(city)
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 public class ProfilesTest {
 
     @Test
-    public void whenLinearFunctionThenLinearResults() {
+    public void whenCollectAddresses() {
         Address firstAd = new Address("Moscow", "Lenina", 5, 5);
         Address secondAd = new Address("Yekaterinburg", "Gagarina", 28, 10);
         Address thirdAd = new Address("Moscow", "Lenina", 5, 5);
@@ -28,4 +28,19 @@ public class ProfilesTest {
         assertThat(result, is(expected));
     }
 
+    @Test
+    public void whenCollectUniqueAddresses() {
+        Address firstAd = new Address("Moscow", "Lenina", 5, 5);
+        Address secondAd = new Address("Yekaterinburg", "Gagarina", 28, 10);
+        Address thirdAd = new Address("Moscow", "Lenina", 5, 5);
+        List<Profile> profilesList = new ArrayList<>();
+        profilesList.add(new Profile(firstAd));
+        profilesList.add(new Profile(secondAd));
+        profilesList.add(new Profile(thirdAd));
+        Profiles profiles = new Profiles();
+        List<Address> result = profiles.collectUnique(profilesList);
+        List<Address> expected = Arrays.asList(new Address("Moscow", "Lenina", 5, 5),
+                new Address("Yekaterinburg", "Gagarina", 28, 10));
+        assertThat(result, is(expected));
+    }
 }

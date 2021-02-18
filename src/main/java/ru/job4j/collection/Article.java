6fafa83721b4726042da814be8de7c35b1737b17
Azat Idrisov <azzzat72@yandex.ru>
2021-02-18 16:31:43 +0500

    1. Экзамен. GC. [#348818]

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -1,6 +1,7 @@
 package ru.job4j.collection;
 
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,6 +16,28 @@ public class Article {
         }
         for (String checkingWord : text) {
             if (!uniqueWords.contains(checkingWord)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean generateByWithHashMap(String origin, String line) {
+        boolean result = true;
+        String[] words = origin.split(" ");
+        String[] text = line.split(" ");
+        HashMap<String, Integer> wordsFrequency = new HashMap<>();
+        for (String originWord : words) {
+            if (wordsFrequency.containsKey(originWord)) {
+                wordsFrequency.put(originWord.replaceAll("\\p{P}", ""),
+                        wordsFrequency.get(originWord) + 1);
+            } else {
+                wordsFrequency.put(originWord.replaceAll("\\p{P}", ""), 1);
+            }
+        }
+        for (String checkingWord : text) {
+            if (!wordsFrequency.containsKey(checkingWord)) {
                 result = false;
                 break;
             }
--- src/test/java/ru/job4j/collection/ArticleJMHTest.java
+++ src/test/java/ru/job4j/collection/ArticleJMHTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.collection;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openjdk.jmh.annotations.*;
@@ -41,6 +40,51 @@ public class ArticleJMHTest {
     public void jmhTestWhenLongText() {
         assertThat(
                 Article.generateBy(
+                        "Мой дядя самых честных правил, "
+                                + "Когда не в шутку занемог, "
+                                + "Он уважать себя заставил "
+                                + "И лучше выдумать не мог. "
+                                + "Его пример другим наука; "
+                                + "Но, боже мой, какая скука "
+                                + "С больным сидеть и день и ночь, "
+                                + "Не отходя ни шагу прочь! "
+                                + "Какое низкое коварство "
+                                + "Полуживого забавлять, "
+                                + "Ему подушки поправлять, "
+                                + "Печально подносить лекарство, "
+                                + "Вздыхать и думать про себя: "
+                                + "Когда же черт возьмет тебя!",
+                        "Мой дядя мог думать про тебя и день и ночь"
+                ),
+                is(true)
+        );
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.All)
+    @Fork(value = 1)
+    @Warmup(iterations = 1)
+    @Measurement(iterations = 10)
+    @Test
+    public void jmhTestWhenShortTextWithHashMap() {
+        assertThat(
+                Article.generateByWithHashMap(
+                        "Мама мыла раму и окно",
+                        "мыла пол"
+                ),
+                is(false)
+        );
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.All)
+    @Fork(value = 1)
+    @Warmup(iterations = 1)
+    @Measurement(iterations = 10)
+    @Test
+    public void jmhTestWhenLongTextWithHashMap() {
+        assertThat(
+                Article.generateByWithHashMap(
                         "Мой дядя самых честных правил, "
                                 + "Когда не в шутку занемог, "
                                 + "Он уважать себя заставил "
